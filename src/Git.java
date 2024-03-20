cbce73fae61f8e4320387ef715aa3b70419b9152
ST10442433 <zikho@Zikhona_PC>
2024-03-20 11:23:02 +0200

    Work

---
+++ src/Git.java
@@ -0,0 +1,14 @@
+
+public class Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+		int y = 10;
+		int z = x+y;
+		System.out.println(z);
+		
+		
+	}
+
+}
